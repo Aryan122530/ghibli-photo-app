0ba8a81cd46e13b457897a68702645ea5cab2b4b
Aryan122530 <solankiaryansolankiashish@gmail.com>
2025-03-31 11:16:39 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import GhibliStylePhotoApp from "./GhibliStylePhotoApp";
+
+ReactDOM.render(<GhibliStylePhotoApp />, document.getElementById("root"));
